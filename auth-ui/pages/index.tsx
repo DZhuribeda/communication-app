8346200d28296ca68e489542121221ddf66ed83d
Max Kindritskiy <kindritskiy.m@gmail.com>
2021-06-11 11:05:58 +0300

    setup simple ws client/server

--- auth-ui/pages/index.tsx
+++ auth-ui/pages/index.tsx
@@ -1,7 +1,57 @@
 import { GetServerSidePropsContext } from "next";
 import Link from "next/link";
+import React from "react";
 import config from "../lib/config";
 import { getCurrentUserIdentity } from "../lib/kratos";
+
+
+
+type NewSocketOpts = {
+  onMessage: (event: MessageEvent) => void;
+};
+
+const newSocket = (addr: string, opts: NewSocketOpts): WebSocket => {
+  console.log(opts);
+  const socket = new WebSocket(addr);
+  socket.onopen = () => {
+      console.log("Successfully Connected");
+  };
+
+  socket.onclose = event => {
+      console.log("Socket Closed Connection: ", event);
+  };
+
+  socket.onerror = error => {
+      console.log("Socket Error: ", error);
+  };
+
+  socket.onmessage = (event) => {
+    opts.onMessage(event);
+  }
+
+  return socket;
+}
+
+const WsView = () => {
+  const [socket, setSocket] = React.useState(null);
+  const [msg, setMsg] = React.useState<MessageEvent>(null);
+
+  React.useEffect(() => {
+    setSocket(newSocket('ws://127.0.0.1:8080/ws', {
+      onMessage: setMsg
+  }));
+  }, []);
+
+
+  const sendPing = () => socket.send('Ping!');
+
+  return (
+    <div>
+      <button type="button" onClick={sendPing}>Ping WS</button>
+      {msg && <div>{msg.data}</div>}
+    </div>
+  );
+}
 
 export default function Home({ user }) {
   return (
@@ -10,6 +60,7 @@ export default function Home({ user }) {
         Welcome{" "}
         {user ? `${user?.traits.name.first} ${user?.traits.name.last}` : null}
       </div>
+      <WsView />
       {user ? (
         <a href={`${config.kratos.browser}/self-service/browser/flows/logout`}>
           Log out
--- ws/main.go
+++ ws/main.go
@@ -4,15 +4,55 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
 
-func IndexHanlder(w http.ResponseWriter, r *http.Request) {
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello")
+}
+
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, []byte(fmt.Sprintf("Pong! | %d", time.Now().Unix()))); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client Connected")
+
+	reader(ws)
 }
 
 func main() {
 	fmt.Println("Ws server")
 
-	http.HandleFunc("/", IndexHanlder)
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/ws", wsHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
