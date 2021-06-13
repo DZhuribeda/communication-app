f8766d4827a750f856654f121908a09ba5d93bcf
DZhuribeda <zhuribedadb@gmail.com>
2021-06-13 14:47:17 +0300

    add lint to check it

--- messages/src/app.ts
+++ messages/src/app.ts
@@ -4,6 +4,11 @@ import express from "express";
 export async function createApp() {
   const app = express();
 
+  
+
+
+
+  
   /**
    * A little hack here
    * Import/Export can only be used in 'top-level code'
