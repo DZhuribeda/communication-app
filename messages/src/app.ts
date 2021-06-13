150bfab5c8fe2a4f5b11eecaa2e8bf824198a11f
DZhuribeda <DZhuribeda@users.noreply.github.com>
2021-06-13 11:47:50 +0000

    Apply formatting changes

--- messages/src/app.ts
+++ messages/src/app.ts
@@ -4,11 +4,6 @@ import express from "express";
 export async function createApp() {
   const app = express();
 
-  
-
-
-
-  
   /**
    * A little hack here
    * Import/Export can only be used in 'top-level code'
