4f45c7771cb6e3e9f0df291dfd3a5bcc93013f42
DZhuribeda <zhuribedadb@gmail.com>
2021-06-16 10:05:24 +0300

    setup material ui

--- frontend/lib/session.ts
+++
@@ -1,7 +0,0 @@
-import { GetServerSidePropsContext } from 'next';
-
-export async function withSession(next: (GetServerSidePropsContext) => void) {
-    return (context: GetServerSidePropsContext) => {
-        
-    }
-}
