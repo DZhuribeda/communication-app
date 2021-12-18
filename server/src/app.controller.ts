b2eb50f44f6d302fc2067fb6501c5103af513053
DZhuribeda <zhuribedadb@gmail.com>
2021-12-18 21:24:30 +0200

    setup nest server

---
+++ server/src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('api/v1')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
