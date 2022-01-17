38a12224af7aed7655ace3db03035ce182d1030c
DZhuribeda <zhuribedadb@gmail.com>
2022-01-17 10:14:21 +0200

    prettier

--- frontend/components/core/notification/notification.tsx
+++ frontend/components/core/notification/notification.tsx
@@ -1,24 +1,27 @@
-import { Toaster, ToastBar, toast, ToastOptions } from 'react-hot-toast';
-import { CheckIcon, ExclamationCircleIcon, ExclamationIcon } from '@heroicons/react/outline';
-
+import { Toaster, ToastBar, toast, ToastOptions } from "react-hot-toast";
+import {
+  CheckIcon,
+  ExclamationCircleIcon,
+  ExclamationIcon,
+} from "@heroicons/react/outline";
 
 export function SuccessNotificationIcon() {
   return (
-    <div className='p-1 rounded-full bg-success-100 border-4 border-success-50'>
+    <div className="p-1 rounded-full bg-success-100 border-4 border-success-50">
       <CheckIcon className="h-4 w-4" />
     </div>
   );
-} 
+}
 export function ErrorNotificationIcon() {
   return (
-    <div className='p-1 rounded-full bg-error-100 border-4 border-error-50'>
+    <div className="p-1 rounded-full bg-error-100 border-4 border-error-50">
       <ExclamationCircleIcon className="h-4 w-4" />
     </div>
   );
 }
 export function WarningNotificationIcon() {
   return (
-    <div className='p-1 rounded-full bg-warning-100 border-4 border-warning-50'>
+    <div className="p-1 rounded-full bg-warning-100 border-4 border-warning-50">
       <ExclamationIcon className="h-4 w-4" />
     </div>
   );
@@ -32,7 +35,7 @@ export function Notification() {
           {({ icon, message }) => (
             <>
               {icon}
-              <span className='text-sm font-medium text-gray-900'>
+              <span className="text-sm font-medium text-gray-900">
                 {message}
               </span>
             </>
@@ -47,11 +50,9 @@ export function success(message: string, options?: ToastOptions) {
   toast.success(message, { icon: <SuccessNotificationIcon />, ...options });
 }
 
-
 export function error(message: string, options?: ToastOptions) {
   toast.error(message, { icon: <ErrorNotificationIcon />, ...options });
 }
-
 
 export function warning(message: string, options?: ToastOptions) {
   toast(message, { icon: <WarningNotificationIcon />, ...options });
--- frontend/stories/Notification.stories.tsx
+++ frontend/stories/Notification.stories.tsx
@@ -2,7 +2,12 @@ import React from "react";
 
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 
-import { Notification, success, error, warning } from "../components/core/notification/notification";
+import {
+  Notification,
+  success,
+  error,
+  warning,
+} from "../components/core/notification/notification";
 
 export default {
   title: "Notification",
@@ -19,17 +24,17 @@ const Template: ComponentStory<typeof Notification> = (args) => (
 export const Success = Template.bind({});
 
 Success.args = {
-  action: () => success('Hey', { duration: 400000000, }),
+  action: () => success("Hey", { duration: 400000000 }),
 };
 
 export const Error = Template.bind({});
 
 Error.args = {
-  action: () => error('Hey', { duration: 400000000, }),
+  action: () => error("Hey", { duration: 400000000 }),
 };
 
 export const Warning = Template.bind({});
 
 Warning.args = {
-  action: () => warning('Hey', { duration: 400000000, }),
+  action: () => warning("Hey", { duration: 400000000 }),
 };
